3d81ddc4dfdfde3c574887c8e37db059b72a33ec
Mert Koksal <mertkoksal88@gmail.com>
2025-10-14 18:47:17 +0300

    Initial Commit

---
+++ HUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HUD
+//
+//  Created by Mert Köksal on 14.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HUD/HUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUDApp.swift
+//  HUD
+//
+//  Created by Mert Köksal on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct HUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
